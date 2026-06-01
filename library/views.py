864c6b9884f3895fc3d8b912fc4baa771c86601a
Vansh Jaiswal <vjas2606@gmail.com>
2026-06-01 18:49:02 +0530

    updated

--- library/views.py
+++ library/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'library/home.html')
 
 def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        rollno = request.POST.get("rollno")
+
+        if not email.endswith("@thapar.edu"):
+            return render(request, "library/login.html", {
+                "error": "Please enter a valid Thapar email ID."
+            })
+
+        if not (rollno.isdigit() and len(rollno) == 10):
+            return render(request, "library/login.html", {
+                "error": "Roll number must contain exactly 10 digits."
+            })
+
+        return redirect('index')
     return render(request, 'library/login.html')
 
 def index_view(request):
