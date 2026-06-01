80407b0b83b35836b2d44ff1a644c5353ba631c0
Vansh Jaiswal <vjas2606@gmail.com>
2026-06-01 17:24:12 +0530

    adding other files

--- library/views.py
+++ library/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'library/home.html')
+
+def login_view(request):
+    return render(request, 'library/login.html')
+
+def index_view(request):
+    return render(request, 'library/index.html')
